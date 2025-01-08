dd023bd6a056d3d1be4edf2839002a7716c43991
jxt <jjt1525@outlook.com>
2025-01-08 15:46:47 +0800

    update

--- src/task/github.rs
+++ src/task/github.rs
@@ -1,14 +1,12 @@
-use std::sync::Arc;
+use std::{future::Future, pin::Pin, sync::Arc};
 
 use anyhow::Result;
-use async_trait::async_trait;
 use tera::Context;
 
 use super::{Helper, Task};
 
 pub struct Github;
 
-#[async_trait]
 impl Task for Github {
     fn job(&self) -> &'static str {
         "0 0 6 * * *"
@@ -18,23 +16,25 @@ impl Task for Github {
         "get github daily trending"
     }
 
-    async fn run(&self, helper: Arc<Helper>) -> Result<()> {
-        let url = "https://github.com/trending/rust?since=daily";
-        let trending = helper
-            .jina
-            .summary(url, [".Box-row"], [".d-inline-block"])
-            .await?;
-        let summary = helper
-            .openai
-            .chat(format!(
-                "请帮我总结一下所有的项目并按照顺序star的多少排列, {trending}"
-            ))
-            .await?;
-        let mut context = Context::new();
-        context.insert("trending", &markdown::to_html(&summary));
-        let html = helper.tera.render("github.html", &context)?;
-        helper.mailer.send("Github rust trending", html).await?;
+    fn run(&self, helper: Arc<Helper>) -> Pin<Box<dyn Future<Output = Result<()>> + '_ + Send>> {
+        Box::pin(async move {
+            let url = "https://github.com/trending/rust?since=daily";
+            let trending = helper
+                .jina
+                .summary(url, [".Box-row"], [".d-inline-block"])
+                .await?;
+            let summary = helper
+                .openai
+                .chat(format!(
+                    "请帮我总结一下所有的项目并按照顺序star的多少排列, {trending}"
+                ))
+                .await?;
+            let mut context = Context::new();
+            context.insert("trending", &markdown::to_html(&summary));
+            let html = helper.tera.render("github.html", &context)?;
+            helper.mailer.send("Github rust trending", html).await?;
 
-        Ok(())
+            Ok(())
+        })
     }
 }
--- src/task/mod.rs
+++ src/task/mod.rs
@@ -1,15 +1,13 @@
 pub mod github;
 
-use std::sync::Arc;
+use std::{future::Future, pin::Pin, sync::Arc};
 
 use anyhow::Result;
-use async_trait::async_trait;
 
 use super::helper::Helper;
 
-#[async_trait]
-pub trait Task: Send + Sync {
+pub trait Task: Send + Sync + 'static {
     fn job(&self) -> &'static str;
     fn description(&self) -> &'static str;
-    async fn run(&self, helper: Arc<Helper>) -> Result<()>;
+    fn run(&self, helper: Arc<Helper>) -> Pin<Box<dyn Future<Output = Result<()>> + '_ + Send>>;
 }
